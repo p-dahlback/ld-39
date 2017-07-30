5337a70d852a4b5345946820d9873a90919bc059
Peter Dahlbäck <Peter@Peters-MBP.mshome.net>
2017-07-30 15:33:10 +0200

    Added start message

---
+++ Assets/Scripts/Utilities/AutoDisable.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoDisable : MonoBehaviour {
+
+	public float disableTime = 1f;
+
+	private float time;
+
+	void OnEnable () {
+		time = 0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		time += Time.deltaTime;
+
+		if (time >= disableTime) {
+			gameObject.SetActive(false);
+		}
+	}
+}
---
+++ Assets/Scripts/Utilities/FlashingGraphics.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FlashingGraphics : MonoBehaviour {
+
+	public MaskableGraphic graphics;
+	public float flashFrequency = 4f;
+
+	private float time = 0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var flashTime = 1.0f / flashFrequency;
+		time += Time.deltaTime;
+		time %= flashTime;
+
+		graphics.enabled = time <= flashTime / 2f;
+	}
+}
