3ca2d78f3a1e02b1ee0ce182081a08a5982d57a0
Peter Dahlbäck <Peter@Peters-MacBook-Pro.local>
2017-07-31 13:23:44 +0200

    Added splash animation when on water

---
+++ Assets/Scripts/Entities/ShipSplash.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShipSplash : MonoBehaviour
+{
+	public Transform contact;
+	public float minimumDistanceToContact = 0.5f;
+	public float targetYPosition = 0f;
+	public float yPositionChangeRate = 2f;
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (contact == null || !contact.gameObject.activeInHierarchy) {
+			Debug.Log("Contact gone; destroying self");
+			DestroySelf();
+			return;
+		}
+
+		var contactPosition = contact.position;
+		if (contactPosition.y > minimumDistanceToContact) {
+			Debug.Log("Contact lifted; destroying self");
+			DestroySelf();
+			return;
+		}
+
+		var position = contactPosition;
+		position.y = transform.position.y;
+		transform.position = position;
+
+		ApproachTargetYPosition();
+	}
+
+	private void ApproachTargetYPosition() {
+		var difference = targetYPosition - transform.position.y;
+		float newPos;
+		if (Mathf.Abs(difference) > 0.01) {
+			newPos = transform.position.y + difference * Time.deltaTime;
+		} else {
+			newPos = targetYPosition;
+		}
+		var position = transform.position;
+		position.y = newPos;
+		transform.position = position;
+	}
+
+	private void DestroySelf() {
+		Destroy(gameObject);
+	}
+}
+
---
+++ Assets/Scripts/Entities/SplashDetector.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SplashDetector : MonoBehaviour
+{
+	public Transform shipSplashPrefab;
+	public Transform fallSplashPrefab;
+	public Transform splashContainer;
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+
+	void OnTriggerEnter(Collider collider) {
+		Transform splash;
+
+		if (collider.gameObject.layer == (int) Layers.Player) {
+			splash = Instantiate(shipSplashPrefab, splashContainer);
+			var shipSplash = splash.GetComponent<ShipSplash>();
+			shipSplash.contact = collider.transform;
+			Debug.Log("Ship splash!");
+		} else {
+			splash = Instantiate(fallSplashPrefab, splashContainer);
+			Debug.Log("Other splash!");
+		}
+		var position = splash.position;
+		position.x = collider.transform.position.x;
+		position.z = collider.transform.position.z;
+		splash.position = position;
+	}
+}
+
